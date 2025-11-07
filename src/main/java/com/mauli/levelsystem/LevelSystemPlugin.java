b542b4ae7d7e19ff17df0346c3037137578ea41b
liago1234567 <gleimliam6@gmail.com>
2025-11-07 11:04:17 +0100

    Update LevelSystemPlugin.java

--- src/main/java/com/mauli/levelsystem/LevelSystemPlugin.java
+++ src/main/java/com/mauli/levelsystem/LevelSystemPlugin.java
@@ -9,26 +9,37 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class LevelSystemPlugin extends JavaPlugin {
 
     private static LevelSystemPlugin instance;
+    private DataStore store;
+    private PlaytimeTracker playtime;
+    private LevelGUI gui;
 
-    public static LevelSystemPlugin getInstance() {
-        return instance;
-    }
+    public static LevelSystemPlugin getInstance() { return instance; }
+    public DataStore getStore() { return store; }
+    public LevelGUI getGui() { return gui; }
 
     @Override
     public void onEnable() {
         instance = this;
 
-        saveDefaultConfig();
+        saveDefaultConfig();              // plugin.yml/config.yml bereitstellen
+        store = new DataStore(this);      // Datenverwaltung
+        playtime = new PlaytimeTracker(this);
+        playtime.runTaskTimer(this, 20L, 20L * 60); // jede Minute
 
-        new LevelGUI(this);
+        gui = new LevelGUI(this);
+
         getCommand("level").setExecutor(new LevelCommand(this));
         getCommand("leveladmin").setExecutor(new LevelAdminCommand(this));
+
+        // Optional: VoteListener registrieren (nur nötig, wenn du später NuVotifier nutzt)
+        Bukkit.getPluginManager().registerEvents(new VoteListener(this), this);
 
         getLogger().info("LevelSystem aktiviert.");
     }
 
     @Override
     public void onDisable() {
+        store.saveNow();
         getLogger().info("LevelSystem deaktiviert.");
     }
 }
