52559b83622a6efa098e770ea53cad823ea64644
liago1234567 <gleimliam6@gmail.com>
2025-11-07 17:20:01 +0100

    Refactor LevelSystemPlugin initialization and commands

--- src/main/java/com/mauli/levelsystem/LevelSystemPlugin.java
+++ src/main/java/com/mauli/levelsystem/LevelSystemPlugin.java
@@ -1,44 +1,44 @@
 package com.mauli.levelsystem;
 
-import com.mauli.levelsystem.commands.LevelAdminCommand;
 import com.mauli.levelsystem.commands.LevelCommand;
 import com.mauli.levelsystem.gui.LevelGUI;
+import com.mauli.levelsystem.logic.LevelManager;
+import com.mauli.levelsystem.tracker.PlaytimeTracker;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LevelSystemPlugin extends JavaPlugin {
 
     private static LevelSystemPlugin instance;
+
     private DataStore store;
-    private PlaytimeTracker playtime;
+    private LevelManager levelManager;
     private LevelGUI gui;
 
     public static LevelSystemPlugin getInstance() { return instance; }
     public DataStore getStore() { return store; }
+    public LevelManager getLevelManager() { return levelManager; }
     public LevelGUI getGui() { return gui; }
 
     @Override
     public void onEnable() {
         instance = this;
 
-        saveDefaultConfig();              // plugin.yml/config.yml bereitstellen
-        store = new DataStore(this);      // Datenverwaltung
-        playtime = new PlaytimeTracker(this);
-        playtime.runTaskTimer(this, 20L, 20L * 60); // jede Minute
+        saveDefaultConfig(); // stellt config.yml bereit
+        this.store = new DataStore(this);
+        this.levelManager = new LevelManager(this);
+        this.gui = new LevelGUI(this);
 
-        gui = new LevelGUI(this);
+        // Spielzeit alle 60s hochzählen
+        new PlaytimeTracker(this).runTaskTimer(this, 20L, 20L * 60);
 
+        // /level
         getCommand("level").setExecutor(new LevelCommand(this));
-        getCommand("leveladmin").setExecutor(new LevelAdminCommand(this));
 
-        // Optional: VoteListener registrieren (nur nötig, wenn du später NuVotifier nutzt)
+        // GUI-Listener
+        Bukkit.getPluginManager().registerEvents(gui, this);
 
         getLogger().info("LevelSystem aktiviert.");
-    }
-
-    @Override
-    public void onDisable() {
-        store.saveNow();
-        getLogger().info("LevelSystem deaktiviert.");
     }
 }
