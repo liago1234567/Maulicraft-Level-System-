f45e60312d95216996f0bc8a03a2b0f346d72add
liago1234567 <gleimliam6@gmail.com>
2025-11-08 10:51:06 +0100

    Update LevelSystemPlugin.java

--- src/main/java/com/mauli/levelsystem/LevelSystemPlugin.java
+++ src/main/java/com/mauli/levelsystem/LevelSystemPlugin.java
@@ -9,35 +9,45 @@ import com.mauli.levelsystem.tracker.PlaytimeTracker;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LevelSystemPlugin extends JavaPlugin {
+
     private static LevelSystemPlugin instance;
 
     private DataStore store;
     private LevelManager manager;
     private LevelGUI gui;
-    private PlaytimeTracker tracker;
+    private PlaytimeTracker playtimeTracker;
 
-    public static LevelSystemPlugin getInstance() { return instance; }
+    public static LevelSystemPlugin get() { return instance; }
     public DataStore getStore() { return store; }
     public LevelManager getLevelManager() { return manager; }
     public LevelGUI getGui() { return gui; }
+    public PlaytimeTracker getPlaytimeTracker() { return playtimeTracker; }
 
-    @Override public void onEnable() {
+    @Override
+    public void onEnable() {
         instance = this;
         saveDefaultConfig();
 
         this.store = new DataStore(this);
         this.manager = new LevelManager(this);
         this.gui = new LevelGUI(this);
-        this.tracker = new PlaytimeTracker(this);
+        this.playtimeTracker = new PlaytimeTracker(this);
 
         getCommand("level").setExecutor(new LevelCommand(this));
         getCommand("leveladmin").setExecutor(new LevelAdminCommand(this));
 
-        getLogger().info("LevelSystem aktiviert.");
+        // Optional: Votifier Auto-Vote (nur wenn Plugin installiert ist)
+        if (getServer().getPluginManager().getPlugin("Votifier") != null) {
+            getServer().getPluginManager().registerEvents(new com.mauli.levelsystem.hook.VoteListener(this), this);
+            getLogger().info("Votifier erkannt – Votes werden automatisch gezählt.");
+        } else {
+            getLogger().info("Votifier nicht gefunden – Votes bitte per Command setzen (oder VotingPlugin-Hook nutzen).");
+        }
     }
 
-    @Override public void onDisable() {
-        if (tracker != null) tracker.stop();
+    @Override
+    public void onDisable() {
         if (store != null) store.savePlayers();
+        if (playtimeTracker != null) playtimeTracker.stop();
     }
 }
