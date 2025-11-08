cb2f32fc7e784f5b8453291aa2ea4e17e1f8b3db
liago1234567 <gleimliam6@gmail.com>
2025-11-08 10:58:10 +0100

    Update LevelAdminCommand.java

--- src/main/java/com/mauli/levelsystem/commands/LevelAdminCommand.java
+++ src/main/java/com/mauli/levelsystem/commands/LevelAdminCommand.java
@@ -4,9 +4,7 @@ import com.mauli.levelsystem.LevelSystemPlugin;
 import com.mauli.levelsystem.store.DataStore;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
 
 public class LevelAdminCommand implements CommandExecutor {
     private final LevelSystemPlugin plugin;
@@ -22,35 +20,32 @@ public class LevelAdminCommand implements CommandExecutor {
 
         if (a.length == 1 && a[0].equalsIgnoreCase("reload")) {
             plugin.reloadConfig();
-            s.sendMessage("§aConfig neu geladen.");
+            s.sendMessage(plugin.getConfig().getString("messages.reloaded","§aConfig neu geladen."));
             return true;
         }
 
-        if (a.length == 3 && (a[0].equalsIgnoreCase("setvotes") || a[0].equalsIgnoreCase("setminutes"))) {
+        if (a.length == 3 && (a[0].equalsIgnoreCase("setminutes") || a[0].equalsIgnoreCase("setvotes"))) {
             OfflinePlayer op = Bukkit.getOfflinePlayer(a[1]);
-            int num;
-            try { num = Integer.parseInt(a[2]); } catch (Exception e){ s.sendMessage("§cZahl ungültig."); return true; }
-
-            if (a[0].equalsIgnoreCase("setvotes")) store.setVotes(op.getUniqueId(), num);
-            else store.setMinutes(op.getUniqueId(), num);
-
+            int num; try { num = Integer.parseInt(a[2]); } catch (Exception e) { s.sendMessage("§cZahl ungültig."); return true; }
+            if (a[0].equalsIgnoreCase("setminutes")) store.setMinutes(op.getUniqueId(), num);
+            else store.setVotes(op.getUniqueId(), num);
             store.savePlayers();
-            s.sendMessage("§aGesetzt für "+op.getName()+".");
+            s.sendMessage("§aGesetzt für " + op.getName() + ".");
             return true;
         }
 
-        if (a.length == 2 && a[0].equalsIgnoreCase("reset")) {
+        if (a.length == 2 && a[0].equalsIgnoreCase("resetclaims")) {
             OfflinePlayer op = Bukkit.getOfflinePlayer(a[1]);
             store.resetClaims(op.getUniqueId());
             store.savePlayers();
-            s.sendMessage("§aClaims von "+op.getName()+" zurückgesetzt.");
+            s.sendMessage("§aClaims von " + op.getName() + " zurückgesetzt.");
             return true;
         }
 
         s.sendMessage("§e/leveladmin reload");
+        s.sendMessage("§e/leveladmin setminutes <Spieler> <Minuten>");
         s.sendMessage("§e/leveladmin setvotes <Spieler> <Zahl>");
-        s.sendMessage("§e/leveladmin setminutes <Spieler> <Zahl>");
-        s.sendMessage("§e/leveladmin reset <Spieler>");
+        s.sendMessage("§e/leveladmin resetclaims <Spieler>");
         return true;
     }
 }
